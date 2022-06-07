f8c39c5ee99b044005dd1a733ddeea87e7a762cf
Ian Okumu <okumu.otsembo@gmail.com>
2022-06-07 10:18:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+// UTILITY
+function printValue(value){
+    console.info(value)
+}
+
+// 1.0 DEFINE VARIABLES
+//TODO: ADD fixed variable
+
+
+//TODO: ADD redeclarable variable
+
+
+//TODO: ADD re-assignable but not redeclarable variable
+
+
+// 2.0 DIFFERENT DATA TYPES
+//TODO: Create a fixed variable with a number value
+
+
+//TODO: Create a redeclarable variable with a boolean value
+
+
+//TODO: Create a re-assignable variable with a string value
+
+
+
+// 3.0 STRING OPERATIONS
+//TODO: Concatenate string (+)
+
+
+//TODO: Interpolate string (`${}`)
+
+
+//TODO: Convert to uppercase
+
+
+//TODO: Convert to lowercase
+
+
+//TODO: Index a specific character
+
+
+//TODO: PRACTICE ON YOUR OWN (Check mozilla documentation)
+
+
+// 4.0 COMPARISON
+//TODO: strict equality
+
+
+//TODO: strict inequality
+
+
+//TODO: equality
+
+
+//TODO: inequality
+
+
+//TODO: greater than
+
+
+//TODO: less than
+
+
+//TODO: greater or equal to
+
+
+//TODO: less or equal to
+
+
+
+// 5.0 CONTROL FLOW
+// TODO: if-else statement
+
+
+//TODO: switch-case
+
+
+
+
+
+
