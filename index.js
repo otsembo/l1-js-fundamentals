82a0916a1b9c2aa07556af41091b91b96670151a
Ian Okumu <okumu.otsembo@gmail.com>
2022-06-07 16:23:11 +0300

    Completed afternoon classes

--- index.js
+++ index.js
@@ -5,67 +5,94 @@ function printValue(value){
 
 // 1.0 DEFINE VARIABLES
 //TODO: ADD fixed variable
-
+const age = 55
+// printValue(age)
 
 //TODO: ADD redeclarable variable
+var name = "James"
+var name = "Wanyoike"
+//printValue(name)
 
 
 //TODO: ADD re-assignable but not redeclarable variable
+let destination = "Nakuru"
+//let name = "Ian"
+//printValue(name)
 
 
 // 2.0 DIFFERENT DATA TYPES
 //TODO: Create a fixed variable with a number value
+const distance = 20
+//printValue(distance)
+
+//TODO: Create a redeclarable variable with a boolean value (false/true)
+var zero = false
+//printValue(zero)
 
 
-//TODO: Create a redeclarable variable with a boolean value
-
-
-//TODO: Create a re-assignable variable with a string value
+//TODO: Create a re-assignable variable with a string value 
+// (`value`, "value", 'value')
+let city = 'Nairobi'
+//printValue(city)
 
 
 
 // 3.0 STRING OPERATIONS
 //TODO: Concatenate string (+)
+const fname = 'Ian'
+const lname = 'Okumu'
+const full_name = fname + " " + lname
+//printValue(full_name)
 
 
 //TODO: Interpolate string (`${}`)
-
+const other_name = `${fname} ${lname}`
+//printValue(other_name)
+// Name: Ian Okumu is home
+const message = `Name: ${full_name} is home`
+//printValue(message)
 
 //TODO: Convert to uppercase
+const upper = message.toUpperCase()
+//printValue(upper)
 
 
 //TODO: Convert to lowercase
+const lower = message.toLowerCase()
+// printValue(lower)
 
 
 //TODO: Index a specific character
-
+const middleChar = message.charAt(88)
+//printValue(middleChar)
 
 //TODO: PRACTICE ON YOUR OWN (Check mozilla documentation)
 
 
-// 4.0 COMPARISON
-//TODO: strict equality
+// 4.0 COMPARISON (will always result in a boolean expression)
+//TODO: strict equality (===)
+//printValue(5 === '5')
+
+//TODO: strict inequality (!==)
+//printValue(5 !== 5)
+
+//TODO: equality (==)
+//printValue(5 == '5')
+
+//TODO: inequality (!=)
 
 
-//TODO: strict inequality
+//TODO: greater than (>)
+//printValue(5 > 10)
+
+//TODO: less than (<)
 
 
-//TODO: equality
+//TODO: greater or equal to (>=)
+//printValue(5 >= 5)
 
 
-//TODO: inequality
-
-
-//TODO: greater than
-
-
-//TODO: less than
-
-
-//TODO: greater or equal to
-
-
-//TODO: less or equal to
+//TODO: less or equal to (<=)
 
 
 
